70a567cb439df7d774ebb429a2ac6557eef359f7
Ishan <ishanatmuzaffarpur@gmail.com>
2015-04-05 23:21:30 +0530

    Added a NumberPicker sample on the left drawer.

--- app/src/main/java/com/ishan1608/studiohello/MainActivity.java
+++ app/src/main/java/com/ishan1608/studiohello/MainActivity.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
     DrawerLayout mainDrawerLayout;
     LinearLayout mainContainer;
-    FrameLayout leftDrawer;
+    LinearLayout leftDrawer;
     LinearLayout rightDrawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +23,7 @@ public class MainActivity extends Activity {
         // Handle for all four layouts
         mainDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
         mainContainer = (LinearLayout) findViewById(R.id.main_container);
-        leftDrawer = (FrameLayout) findViewById(R.id.left_drawer);
+        leftDrawer = (LinearLayout) findViewById(R.id.left_drawer);
         rightDrawer = (LinearLayout) findViewById(R.id.right_drawer);
 
         // Setting the tags for the drawers
@@ -63,6 +64,17 @@ public class MainActivity extends Activity {
         // Setting toggler on the right drawer's contents
         View headingView = findViewById(R.id.headingContainer);
         headingView.setOnClickListener(new contentToggler());
+
+        // Getting a handle on the number picker on the left drawer
+        NumberPicker passengersCountPicker = (NumberPicker) findViewById(R.id.passengers_count_picker);
+        // Setting the maximum and minimum values
+        passengersCountPicker.setMaxValue(9);
+        passengersCountPicker.setMinValue(0);
+        // Setting current Value
+        passengersCountPicker.setValue(5);
+        // Setting the OnValueChangeListener
+        passengersCountPicker.setOnValueChangedListener(new passengerValueListener(this));
+
     }
 }
 
@@ -80,5 +92,30 @@ class contentToggler implements View.OnClickListener {
         } else {
             view.setVisibility(View.VISIBLE);
         }
+    }
+}
+
+/*
+ * Passenger value change listener
+ * Does whatever is required to be done upon the change of value
+ */
+class passengerValueListener implements NumberPicker.OnValueChangeListener {
+    Activity activity;
+    TextView passengersCountDisplay;
+    public passengerValueListener(Activity activity) {
+        super();
+        this.activity = activity;
+        passengersCountDisplay = (TextView) this.activity.findViewById(R.id.passengers_count_display);
+    }
+
+    @Override
+    public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
+        CharSequence previousText = passengersCountDisplay.getText();
+        // Making sure the text doesn't exceeds reasonable length
+        if(previousText.length() >= 40) {
+            passengersCountDisplay.setText(previousText.subSequence(3, previousText.length()));
+        }
+        // Appending the current value
+        passengersCountDisplay.append("--"+newVal);
     }
 }
